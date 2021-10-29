f6646f739ab755e24989cd7b1e3a65bd06b506cd
NicolasIceberg <52626965+NicolasIceberg@users.noreply.github.com>
2021-10-30 02:01:45 +0300

    Add vampire animations - working versions.

--- Assets/ActionState.cs
+++ Assets/ActionState.cs
@@ -44,5 +44,6 @@ public class ActionState : StateMachineBehaviour
     override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
     {
         animator.SetInteger("action", 0);
+        animator.SetTrigger("noActionsInProgress");
     }
 }
