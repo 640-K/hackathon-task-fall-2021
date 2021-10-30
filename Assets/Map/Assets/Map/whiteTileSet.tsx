012a78574ca2faaad8c9e5346e8721e802063d71
ArSiu <arsvikh@gmail.com>
2021-10-30 22:22:20 +0300

    Minecraft village NE

--- Assets/Map/Assets/Map/whiteTileSet.tsx
+++ Assets/Map/Assets/Map/whiteTileSet.tsx
@@ -1,4 +1,89 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="whiteTileSet" tilewidth="32" tileheight="32" margin="25" tilecount="1140" columns="30">
  <image source="whiteTileSet.png" width="992" height="1264"/>
+ <tile id="261">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15" y="0" width="17" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="262">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="263">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="15" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="291">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15" y="0" width="17" height="23"/>
+   <object id="2" x="19.0625" y="23.125" width="12.9375" height="5.4375"/>
+   <object id="3" x="23.5625" y="28.5625" width="8.4375" height="3.4375"/>
+  </objectgroup>
+ </tile>
+ <tile id="292">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="293">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="14.875" height="18"/>
+   <object id="3" x="0" y="18.125" width="12.9375" height="7.0625"/>
+   <object id="4" x="0" y="25.3125" width="9.9375" height="6.6875"/>
+  </objectgroup>
+ </tile>
+ <tile id="1044">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23" y="0" width="9" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1045">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1046">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="27" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1056">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="23" width="24" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="1057">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="23" width="22" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="1074">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23" y="0" width="9" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="1075">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="1076">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="27" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="1086">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7" y="0" width="25" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1087">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="23" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
