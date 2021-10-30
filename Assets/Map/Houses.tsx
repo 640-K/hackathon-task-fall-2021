d77162ffe7587bb24a76a74d6bf9485be7ed392b
ElemRoy balkhed <oleguk0509@gmail.com>
2021-10-30 17:09:38 +0300

    Updated map

---
+++ Assets/Map/Houses.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Houses" tilewidth="32" tileheight="32" tilecount="224" columns="16">
+ <image source="buildings.png" width="534" height="467"/>
+</tileset>
