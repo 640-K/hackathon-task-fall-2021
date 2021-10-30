2671274f73f0762b44ab4ca9703fa850bb51b078
ArSiu <arsvikh@gmail.com>
2021-10-30 18:03:36 +0300

    fix

---
+++ Assets/Map2tempFix/Assets/Map/Buildings.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Church" tilewidth="32" tileheight="32" tilecount="224" columns="9">
+ <image source="Church1.png" width="288" height="304"/>
+</tileset>
