9c6ed11108912e50f33ce8018923929355b07d77
ArSiu <arsvikh@gmail.com>
2021-10-30 21:21:59 +0300

    delete prev map

--- Assets/Map/Forest 2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Forest 2" tilewidth="32" tileheight="32" tilecount="323" columns="19">
- <image source="Forest2.png" width="639" height="544"/>
-</tileset>
