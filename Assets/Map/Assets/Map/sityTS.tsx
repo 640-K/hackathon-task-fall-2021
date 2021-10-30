f2fbc59043e50be841f2ec01a68113996cc224fa
ArSiu <arsvikh@gmail.com>
2021-10-30 23:29:57 +0300

    water border

---
+++ Assets/Map/Assets/Map/sityTS.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="sityts" tilewidth="32" tileheight="32" tilecount="1539" columns="27">
+ <image source="sityTs.png" width="864" height="1841"/>
+</tileset>
