41496bdcff461464eb053aae9999501abc19104b
Dionis <den70007.ua@gmail.com>
2021-10-31 14:18:15 +0200

    kamen

--- Assets/Scripts/AI/Mob.cs
+++ Assets/Scripts/AI/Mob.cs
@@ -7,5 +7,31 @@ using UnityEngine.Rendering;
 namespace Entities{
     public class Mob : Entity
     {
+
+        private Renderer _renderer;
+
+        public override void Start()
+        {
+            base.Start();
+            _renderer = avatarAnimator.gameObject.GetComponent<Renderer>();
+            events.onDie.AddListener(Die);
+
+
+        }
+
+
+        void Die()
+        {
+            avatarAnimator.enabled = false;
+            StartCoroutine(Stone());
+        }
+
+        IEnumerator Stone()
+        {
+            _renderer.material.SetFloat("_Range", _renderer.material.GetFloat("_Range") +0.01f);
+            yield return new WaitForEndOfFrame();
+            if (_renderer.material.GetFloat("_Range") < 1)
+                StartCoroutine(Stone());
+        }
     }
 }
