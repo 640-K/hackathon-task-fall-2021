a19dda5d7acceae0a1139d5a76d22e92629b667a
Pitkov Denis <den70007.ua@gmail.com>
2021-10-31 05:45:22 +0200

    added Mob Contoller

--- Assets/Scripts/AI/MobController.cs
+++ Assets/Scripts/AI/MobController.cs
@@ -17,35 +17,108 @@ public class MobController : EntityController
 
     public float followRadius;
 
+    private float postFollowRadius;
+
     public bool alwaysFollowAfterSee;
 
-    public void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
+    float ScaredSpeed;
 
-    protected void ApplyMotion()
+    [Range(0, 10)]
+    public float stopBetween;
+
+    [Range(0, 10)]
+    public float stopAttack;
+
+    public Transform target;
+
+    public Transform localTarget;
+    protected PathNode playerNode;
+
+    bool isAllowNextStep = true;
+
+    public bool isScared = false;
+
+
+    bool isAutoWalk = true;
+    private void Start()
     {
-        if (controlledEntity == null) return;
+        ScaredSpeed = 1.5f * controlledEntity.maxSpeed;
+        postFollowRadius = 3 * followRadius;
+        if (localTarget == null)
+        {
+            GameObject temp = new GameObject();
+            temp.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            localTarget = temp.transform;
+            playerNode = ground.pathfinding.grid.getNodeFromWorldPosition(localTarget.position);
+        }
+        else
+            isAutoWalk = false;
+    }
+    protected bool ApplyMotion()
+    {
+        if (controlledEntity == null) return false;
+        PathNode startNode = ground.pathfinding.grid.getNodeFromWorldPosition(transform.position);
 
         Vector2 motion = Vector2.zero;
-        PathNode startNode = ground.pathfinding.grid.getNodeFromWorldPosition(transform.position);
-        PathNode playerNode = ground.pathfinding.grid.getNodeFromWorldPosition(player.transform.position);
+        if (!alwaysFollow)
+        {
+
+            if (Vector2.Distance(target.position, transform.position) > followRadius)
+            {
+
+                if (controlledEntity.maxSpeed == ScaredSpeed)
+                    controlledEntity.maxSpeed = ScaredSpeed/1.5f;
+
+
+                if (postFollowRadius == followRadius)
+                    followRadius = postFollowRadius / 3;
+
+                playerNode = ground.pathfinding.grid.getNodeFromWorldPosition(localTarget.position);
+                if (isAllowNextStep && isAutoWalk && controlledEntity.maxSpeed != ScaredSpeed)
+                    StartCoroutine(MoveCoroutine());
+            }
+            else
+            {
+
+
+                if (followRadius != postFollowRadius)
+                    followRadius = postFollowRadius;
+
+                if (alwaysFollowAfterSee)
+                    alwaysFollow = true;
+                if (isScared)
+                {
+                    if (isAllowNextStep)
+                    {
+                        isAllowNextStep = false;
+
+                        playerNode = generetePosition();
+                        if (controlledEntity.maxSpeed != ScaredSpeed)
+                            controlledEntity.maxSpeed = ScaredSpeed;
+                    }
+
+                    if (startNode == playerNode)
+                        isAllowNextStep = true;
+                }
+                else
+                    playerNode = ground.pathfinding.grid.getNodeFromWorldPosition(target.position);
+
+
+            }
+            if (!isAutoWalk && Vector2.Distance(localTarget.position, transform.position) < stopBetween && followRadius != postFollowRadius)
+                playerNode = null;
+        }else
+            playerNode = ground.pathfinding.grid.getNodeFromWorldPosition(target.position);
+
+        if (!isScared&&Vector2.Distance(target.position, transform.position) < stopAttack)
+            playerNode = null;
+            // ATTACK
+        
         if (playerNode == null || startNode == null)
         {
             controlledEntity.Move(motion);
-            return;
+            return false;
         }
-
-        if (!alwaysFollow)
-            if (Vector2.Distance(player.transform.position, transform.position) > followRadius)
-            {
-                controlledEntity.Move(motion);
-                return;
-            }
-            else
-                if (alwaysFollowAfterSee)
-                alwaysFollow = true;
         List<PathNode> path = null;
         if (playerNode.isWalkable)
             path = ground.pathfinding.FindPath(startNode.x, startNode.y, playerNode.x, playerNode.y);
@@ -75,8 +148,32 @@ public class MobController : EntityController
                 motion += Vector2.right;
         }
         controlledEntity.Move(motion);
-
+        return startNode == playerNode;
+    }
+    IEnumerator MoveCoroutine()
+    {
+        isAllowNextStep = false;
+        PathNode endNode = generetePosition();
+        localTarget.position = ground.pathfinding.grid.GetWorldPosition(endNode.x, endNode.y);
+        yield return new WaitForSeconds(Random.Range(2, 8));
+        isAllowNextStep = true;
     }
 
+    PathNode generetePosition()
+    {
+        PathNode startNode = ground.pathfinding.grid.getNodeFromWorldPosition(transform.position);
+        PathNode node = ground.pathfinding.grid.GetPathNode(Random.Range(startNode.x - 5, startNode.x + 5), Random.Range(startNode.y - 5, startNode.y + 5));
+        if (node != null && !node.isWalkable)
+            return generetePosition();
+        return node;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (alwaysFollow || !showGizmos)
+            return;
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, followRadius);
+    }
     protected override void Update() => ApplyMotion();
 }
